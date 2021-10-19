36c2ab021f3cb074d8f8f5e8da82cb7a00be48cc
OlivierArgentieri <olivierargentieri@gmail.com>
2021-10-19 11:38:30 +0200

    ok shelf stay in shelfDock when close dcc

--- startup/scripts/123.py
+++ startup/scripts/123.py
@@ -1,26 +1,71 @@
-print("aaaa")
-
 from silex_client.core.context import Context
 import hou
-from hou import shelves
+import os
+
 
 def create_shelf():
-    print("aaaa")
-
-    hou.shelves.beginChangeBlock()
-
+    """
+    Entry point of create shelf
+    """
+    shelf_id = "silex_shelf"
+    shelfset_id = "silex_shelfset"
+    
+    # create shelfset
+    shelfset = None
     # create shelf
-    shelf = hou.shelves.newShelf("silex_shelf")
+    shelf = None
+
+    # update shelf if already exist
+    for shelf_item in hou.shelves.shelves():
+        if shelf_id in str(shelf_item):
+            shelf = hou.shelves.shelves()[shelf_id]
+
+            # clear content of existing shelf
+            for tool_index in range(len(shelf.tools()), 0, -1):
+                shelf.tools()[tool_index-1].destroy()
+
+    if not shelf:
+        shelf = hou.shelves.newShelf(name=shelf_id, label=shelf_id)
+
+    # find silex shelfset
+    for shelfset_item in hou.shelves.shelfSets():
+        if shelfset_id in str(shelfset_item):
+            print("Silex shelf_set found")
+            shelfset = hou.shelves.shelfSets()[shelfset_item]
+
+    # create shelfset if not exist
+    if not shelfset:
+        shelfset = hou.shelves.newShelfSet(name=shelfset_id, label=shelfset_id)
+
+    # reload in case of update
+    hou.shelves.reloadShelfFiles()
+
+    # start editing shelves
+    hou.shelves.beginChangeBlock()
+    
+    # to switch to another context
+    task_id = Context.get().metadata.get("task_id", None)
+    if task_id:
+        os.environ["SILEX_TASK_ID"] = task_id
+        Context.get().is_outdated = True
     
     # get action
-    actions = { item["name"]: f"Context.get().get_action('{item['name']}').execute()" for item in Context.get().config.actions }
-    print(actions)
+    actions = { item["name"]: f"from silex_client.core.context import Context;Context.get().get_action('{item['name']}').execute()" for item in Context.get().config.actions }
     shelf_tools = []
     for action in actions:
         tool = hou.shelves.newTool(label=action, name=action, script=actions[action])
         shelf_tools.append(tool)
 
+    # set action in pipeline shelf
     shelf.setTools(list(shelf.tools()) + list(shelf_tools))
+    
+    # add shelve in shelfset
+    silex_shelves = list(shelfset.shelves())
+    silex_shelves.append(shelf)
+    shelfset.setShelves(silex_shelves)
+
+    # end editing shelves
     hou.shelves.endChangeBlock()
+    hou.shelves.reloadShelfFiles()
 
 create_shelf()
--- startup/scripts/456.py
+++ startup/scripts/456.py
@@ -1,7 +1,6 @@
 from silex_client.core.context import Context
 import hou
 import os
-import pathlib
 
 
 def create_shelf():
@@ -9,25 +8,46 @@ def create_shelf():
     Entry point of create shelf
     """
     shelf_id = "silex_shelf"
-
+    shelfset_id = "silex_shelfset"
+    
+    # create shelfset
+    shelfset = None
+    # create shelf
+    shelf = None
 
     # update shelf if already exist
-    for shelf in hou.shelves.shelves():
-        if shelf_id in str(shelf):
-            hou.shelves.shelves()[shelf].destroy()
-            hou.shelves.reloadShelfFiles()
+    for shelf_item in hou.shelves.shelves():
+        if shelf_id in str(shelf_item):
+            shelf = hou.shelves.shelves()[shelf_id]
+
+            # clear content of existing shelf
+            for tool_index in range(len(shelf.tools()), 0, -1):
+                shelf.tools()[tool_index-1].destroy()
+
+    if not shelf:
+        shelf = hou.shelves.newShelf(name=shelf_id, label=shelf_id)
+
+    # find silex shelfset
+    for shelfset_item in hou.shelves.shelfSets():
+        if shelfset_id in str(shelfset_item):
+            print("Silex shelf_set found")
+            shelfset = hou.shelves.shelfSets()[shelfset_item]
+
+    # create shelfset if not exist
+    if not shelfset:
+        shelfset = hou.shelves.newShelfSet(name=shelfset_id, label=shelfset_id)
+
+    # reload in case of update
+    hou.shelves.reloadShelfFiles()
 
     # start editing shelves
     hou.shelves.beginChangeBlock()
     
     # to switch to another context
-    shot_id = Context.get().metadata.get("shot_id", None)
-    if shot_id:
-        os.environ["SILEX_TASK_ID"] = shot_id
+    task_id = Context.get().metadata.get("task_id", None)
+    if task_id:
+        os.environ["SILEX_TASK_ID"] = task_id
         Context.get().is_outdated = True
-
-    # create shelf
-    shelf = hou.shelves.newShelf(name=shelf_id, label=shelf_id)
     
     # get action
     actions = { item["name"]: f"from silex_client.core.context import Context;Context.get().get_action('{item['name']}').execute()" for item in Context.get().config.actions }
@@ -39,6 +59,11 @@ def create_shelf():
     # set action in pipeline shelf
     shelf.setTools(list(shelf.tools()) + list(shelf_tools))
     
+    # add shelve in shelfset
+    silex_shelves = list(shelfset.shelves())
+    silex_shelves.append(shelf)
+    shelfset.setShelves(silex_shelves)
+
     # end editing shelves
     hou.shelves.endChangeBlock()
     hou.shelves.reloadShelfFiles()
