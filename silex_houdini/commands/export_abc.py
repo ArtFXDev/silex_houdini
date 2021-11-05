96b48f1c1db33aca212a1d3b2178bd43adf88753
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-11-05 11:15:48 +0100

    feature: add abc export command

---
+++ silex_houdini/commands/export_abc.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+import typing
+from typing import Any, Dict
+
+from silex_client.action.command_base import CommandBase
+from silex_houdini.utils.dialogs import Dialogs
+
+
+# Forward references
+if typing.TYPE_CHECKING:
+    from silex_client.action.action_query import ActionQuery
+
+import hou
+import os
+import gazu
+import pathlib
+
+
+class ExportABC(CommandBase):
+
+    parameters = {
+        "out_dir": { "label": "Out directory", "type": pathlib.Path, "value": "" },
+        "file_name": { "label": "Out filename", "type": pathlib.Path, "value": "" },
+        "start_frame": { "label": "start frame", "type": int },
+        "end_frame": { "label": "End frame", "type": int }
+    }
+
+    @CommandBase.conform_command()
+    async def __call__(
+        self, upstream: Any, parameters: Dict[str, Any], action_query: ActionQuery
+    ):
+
+        async def export():
+            out_dir: str = parameters.get("out_dir", "D:/")
+            file_name: str = parameters.get("file_name", "D:/")
+            out_path: str = f"{out_dir}{os.path.sep}{os.path.sep}{file_name}"
+
+            # Test output path exist
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+            
+            # get current selection
+            if len(hou.selectedNodes()) == 0:
+                Dialogs().warn("No nodes selected, please select Sop nodes and retry.")
+                raise Exception("No nodes selected, please select Sop nodes and retry.")
+
+            for node in hou.selectedNodes():
+                if node.type().category().name() != "Sop":
+                    Dialogs().warn(f"Action only available with Sop Nodes.\nNode {node.name()} will not be exported!")
+                    return ""
+
+                # create a temporary ROP node
+                abc_rop = hou.node(node.parent().path()).createNode('rop_alembic')
+                abc_rop.parm('filename').set(os.path.join(out_path,node.name())+".abc")
+
+                # Set frame range
+                start = parameters.get("start_frame", "$FSTART")
+                end = parameters.get("end_frame", "$FEND")
+
+                abc_rop.parm('f1').deleteAllKeyframes()
+                abc_rop.parm('f1').set(start)
+
+                abc_rop.parm('f2').deleteAllKeyframes()
+                abc_rop.parm('f2').set(end)
+                
+
+                # link node to object
+                abc_rop.setInput(0, node)
+                abc_rop.parm('execute').pressButton()
+                
+                # remove node
+                abc_rop.destroy()
+            
+            print("Done")
+            # export
+            return out_path
+    
+        await export()
