0da35504411510d52deabf424322fe09ecb4040b
Simon Lambin <slambin@artfx.fr>
2021-11-24 16:21:11 +0100

    Change function signatures to match new requirement

--- silex_houdini/commands/export_vrscene.py
+++ silex_houdini/commands/export_vrscene.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 import typing
 from typing import Any, Dict
 
+import logging
 from silex_client.action.command_base import CommandBase
 from silex_client.utils.parameter_types import SelectParameterMeta
-from silex_client.utils.log import logger
 
 # Forward references
 if typing.TYPE_CHECKING:
@@ -32,7 +32,7 @@ class ExportVrscene(CommandBase):
 
     @CommandBase.conform_command()
     async def __call__(
-        self, upstream: Any, parameters: Dict[str, Any], action_query: ActionQuery
+        self, parameters: Dict[str, Any], action_query: ActionQuery, logger: logging.Logger
     ):
 
         outdir = parameters["file_dir"]
