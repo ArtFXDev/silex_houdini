67e7c93b477abbe425dead9ebca4d25cf775864d
OlivierArgentieri <olivierargentieri@gmail.com>
2021-12-06 10:58:39 +0100

    ok wrapped execute abc

--- silex_houdini/commands/export_abc.py
+++ silex_houdini/commands/export_abc.py
@@ -6,6 +6,7 @@ import logging
 from silex_client.action.command_base import CommandBase
 from silex_client.action.parameter_buffer import ParameterBuffer
 from silex_client.utils.parameter_types import IntArrayParameterMeta
+from silex_houdini.utils.utils import Utils
 
 # Forward references
 if typing.TYPE_CHECKING:
@@ -72,6 +73,23 @@ class ExportABC(CommandBase):
         start_frame = parameters.get("frame_range")[0]
         end_frame = parameters.get("frame_range")[1]
 
+        def export_abc(selected_object, final_filename, start_frame, end_frame):
+            # create a temporary ROP node
+            abc_out = hou.node("/out").createNode("alembic")
+            abc_out.parm("filename").set(final_filename)
+            abc_out.parm("objects").set(selected_object)
+
+            # Set frame range
+            abc_out.parm("trange").set(1)
+            abc_out.parmTuple("f").deleteAllKeyframes()  # Needed
+            abc_out.parmTuple("f").set((start_frame, end_frame, 0))
+
+            # link node to object
+            abc_out.parm("execute").pressButton()
+
+            # remove node
+            abc_out.destroy()
+
         # get current selection
         selected_object = [
             item.path()
@@ -93,9 +111,6 @@ class ExportABC(CommandBase):
         # Test output path exist
         os.makedirs(outdir, exist_ok=True)
 
-        # create a temporary ROP node
-        abc_out = hou.node("/out").createNode("alembic")
-
         # compute final path
         extension = await gazu.files.get_output_type_by_name("abc")
         temp_outfilename = (
@@ -103,12 +118,10 @@ class ExportABC(CommandBase):
             if root_name
             else outdir / f"{outfilename}"
         )
+
         final_filename = str(
             pathlib.Path(temp_outfilename).with_suffix(f".{extension['short_name']}")
         )
-
-        abc_out.parm("filename").set(final_filename)
-        abc_out.parm("objects").set(selected_object)
 
         # Set frame range
         if used_timeline:
@@ -116,17 +129,7 @@ class ExportABC(CommandBase):
             start_frame = range_playbar.x()
             end_frame = range_playbar.y()
 
-        # Set frame range
-        abc_out.parm("trange").set(1)
-        abc_out.parmTuple("f").deleteAllKeyframes()  # Needed
-
-        abc_out.parmTuple("f").set((start_frame, end_frame, 0))
-
-        # link node to object
-        abc_out.parm("execute").pressButton()
-
-        # remove node
-        abc_out.destroy()
+        await Utils.wrapped_execute(action_query, export_abc, selected_object, final_filename, start_frame, end_frame)
 
         # export
         logger.info(f"Done export abc, output paths : {final_filename}")
