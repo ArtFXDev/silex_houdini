1a98541329a7ae913ed9cacc79442d016131f21c
Simon Lambin <slambin@artfx.fr>
2021-12-09 00:08:59 +0100

    Add the increment and save action

--- startup/scripts/123.py
+++ startup/scripts/123.py
@@ -1,6 +1,7 @@
 import hou
 
 import pathlib
+
 from silex_client.utils.files import is_valid_pipeline_path
 from silex_client.core.context import Context
 from create_shelf import create_shelf
@@ -9,16 +10,16 @@ from silex_client.action.action_query import ActionQuery
 Context.get().start_services()
 create_shelf()
 
-save_action = ActionQuery("save")
-save_action.set_parameter("save:save_scene:only_path", True)
-save_action.execute()
+# Add a shortcut for increment and save
+inctement_save_symbol = hou.hotkeys.hotkeySymbol("/Houdini", "increment_save")
+hou.hotkeys.addAssignment(inctement_save_symbol, "alt+shift+s")
 
 # Add a save callback
 def save_callback(event_type):
     if event_type != hou.hipFileEventType.BeforeSave:
         return
 
-    if is_valid_pipeline_path(pathlib.Path(hou.hipFile.path()), "work"):
+    if is_valid_pipeline_path(pathlib.Path(hou.hipFile.path()), "working"):
         return
 
     save_action = ActionQuery("save")
@@ -27,3 +28,8 @@ def save_callback(event_type):
 
 
 hou.hipFile.addEventCallback(save_callback)
+
+# Save the scene one first time
+save_action = ActionQuery("save")
+save_action.set_parameter("save:save_scene:only_path", True)
+save_action.execute()
