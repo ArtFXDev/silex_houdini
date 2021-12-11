3a51acb4c4958044b9a8b4fe025b48fa661cb879
Simon Lambin <slambin@artfx.fr>
2021-12-12 00:53:06 +0100

    Refactor the GetReferences command

--- silex_houdini/commands/get_references.py
+++ silex_houdini/commands/get_references.py
@@ -5,6 +5,7 @@ import fileseq
 import pathlib
 import logging
 import os
+import pathlib
 from typing import TYPE_CHECKING, List, Tuple, Dict, Any, Union
 
 import hou
@@ -19,6 +20,8 @@ from silex_houdini.utils.utils import Utils
 if TYPE_CHECKING:
     from silex_client.action.action_query import ActionQuery
 
+
+References = List[Tuple[List[Union[hou.Parm, hou.HDADefinition]], Union[List[pathlib.Path], pathlib.Path]]]
 
 class GetReferences(CommandBase):
     """
@@ -71,155 +74,35 @@ class GetReferences(CommandBase):
             response["new_path"] = pathlib.Path(response["new_path"])
         return response["new_path"], response["skip"]
 
-    @CommandBase.conform_command()
-    async def __call__(
-        self,
-        parameters: Dict[str, Any],
-        action_query: ActionQuery,
-        logger: logging.Logger,
-    ):
-        referenced_files: List[
-            Tuple[
-                List[Union[hou.Parm, hou.HDADefinition]],
-                Union[List[pathlib.Path], pathlib.Path],
-                List[int],
-            ]
-        ] = []
 
-        scene_references = await Utils.wrapped_execute(action_query, hou.fileReferences)
-        filtered_references = await self.filter_references(await scene_references)
-
-        for parameter, file_path in filtered_references:
-            # Skip the references that are in a locked HDA
-            if isinstance(parameter, hou.Parm):
-                is_locked_lamnda = lambda: parameter.node().isInsideLockedHDA()
-                is_locked = await Utils.wrapped_execute(action_query, is_locked_lamnda)
-                if await is_locked:
-                    continue
-
-            expanded_path = await Utils.wrapped_execute(
-                action_query, hou.expandString, file_path
-            )
-            file_path = pathlib.Path(await expanded_path)
-            try:
-                # Make sure the file path leads to a reachable file
-                skip = False
-                while not file_path.exists() or not file_path.is_absolute():
-                    file_path, skip = await self._prompt_new_path(
-                        action_query, file_path, parameter
-                    )
-                    if skip or file_path is None:
-                        break
-                # The user can decide to skip the references that are not reachable
-                if skip or file_path is None:
-                    logger.info("Skipping the reference at %s", parameter)
-                    continue
-
-            except OSError:
-                sequences = fileseq.findSequencesOnDisk(str(file_path.parent))
-                for sequence in sequences:
-                    if str(sequence.basename()) in str(file_path):
-                        file_path = pathlib.Path(str(sequence[0]))
-                        break
-
-            # Skip the references that are already conformed
-            if parameters["skip_conformed"]:
-                if is_valid_pipeline_path(file_path):
-                    continue
-
-            # Initialize the index to -1, which is the value if there is no sequences
-            index = -1
-
-            # Get the definition of the HDA for HDA references
-            if parameter is None and file_path.suffix in [
-                ".hda",
-                ".hdanc",
-                ".hdalc",
-            ]:
-                definitions = await Utils.wrapped_execute(
-                    action_query, hou.hda.definitionsInFile, file_path.as_posix()
-                )
-                for definition in await definitions:
-                    if file_path in [
-                        referenced_file[1] for referenced_file in referenced_files
-                    ]:
-                        break
-                    logger.info("Referenced HDA %s found at %s", file_path, definition)
-                    referenced_files.append(([definition], file_path, [index]))
-                continue
-
-            # Look for a file sequence
-            sequence = None
-            for file_sequence in fileseq.findSequencesOnDisk(str(file_path.parent)):
-                # Find the file sequence that correspond the to file we are looking for
-                sequence_list = [pathlib.Path(str(file)) for file in file_sequence]
-                if file_path in sequence_list and len(sequence_list) > 1:
-                    sequence = file_sequence
-                    index = sequence_list.index(file_path)
-                    file_path = sequence_list
-                    break
-
-            # Append to the verified path
-            referenced_files.append(([parameter], file_path, [index]))
-            if sequence is None:
-                logger.info("Referenced file(s) %s found at %s", file_path, parameter)
-            else:
-                logger.info("Referenced file(s) %s found at %s", sequence, parameter)
-
-        # Remove all the duplicates
+    async def _filter_references(self, references: List[Tuple[Any, patlib.Path]], skip_conformed = True):
+        """
+        Filter out all the references that we don't care about
+        """
         filtered_references = []
-        for reference in referenced_files:
-            file_paths = [file[1] for file in filtered_references]
-            try:
-                index = file_paths.index(reference[1])
-                filtered_references[index][0].extend(reference[0])
-                filtered_references[index][2].extend(reference[2])
-            except ValueError:
-                filtered_references.append(reference)
 
-        # Display a message to the user to inform about all the references to conform
-        referenced_file_paths = [
-            fileseq.findSequencesInList(reference[1])
-            if isinstance(reference[1], list)
-            else [reference[1]]
-            for reference in filtered_references
-        ]
-        message = f"The scene\n{hou.hipFile.path()}\nis referencing non conformed file(s) :\n\n"
-        for file_path in referenced_file_paths:
-            message += f"- {' '.join([str(f) for f in file_path])}\n"
-
-        message += "\nThese files must be conformed and repathed first. Press continue to conform and repath them"
-        info_parameter = ParameterBuffer(
-            type=TextParameterMeta("warning"),
-            name="info",
-            label="Info",
-            value=message,
-        )
-        # Send the message to the user
-        if referenced_file_paths:
-            await self.prompt_user(action_query, {"info": info_parameter})
-
-        return {
-            "attributes": [file[0] for file in filtered_references],
-            "file_paths": [file[1] for file in filtered_references],
-            "indexes": [file[2] for file in filtered_references],
-        }
-
-    async def filter_references(self, references):
-        filtered_references = []
         for parameter, file_path in references:
             if isinstance(parameter, hou.Parm):
+                # Get the node the parameter belongs to
                 node = parameter.node()
 
+                # Skip the references that are in a locked HDA
+                if node().isInsideLockedHDA():
+                    continue
+
                 # Skip TOP network nodes
-                if parameter.node().type().category().name() == "TopNet":
+                if node().type().category().name() == "TopNet":
                     continue
                 # Skip TOP nodes
-                if parameter.node().type().category().name() == "Top":
+                if node().type().category().name() == "Top":
                     continue
 
                 # Skip hidden/disabled parameters
                 if parameter.isDisabled() or parameter.isHidden():
+                    continue
+
+                # Skip channel references
+                if parameter.getReferencedParm() != parameter:
                     continue
 
                 # Skip hidden/disabled containing folders
@@ -235,10 +118,7 @@ class GetReferences(CommandBase):
                         ):
                             continue
 
-                # Skip channel references
-                if parameter.getReferencedParm() != parameter:
-                    continue
-
+                # Get the real path
                 file_path = parameter.eval()
 
             # Skip invalid path
@@ -248,6 +128,10 @@ class GetReferences(CommandBase):
             # Skip path relative
             if not pathlib.Path(file_path).is_absolute():
                 continue
+
+            # Skip the references that are already conformed
+            if skip_conformed and is_valid_pipeline_path(file_path):
+                    continue
 
             # Skip path relative to HOUDINI_PATH
             houdini_path_relative = False
@@ -262,3 +146,106 @@ class GetReferences(CommandBase):
             filtered_references.append((parameter, file_path))
 
         return filtered_references
+
+    
+    def _merge_duplicates(self, references: References):
+        """
+        Merge the files referenced multiple times into one item in the list of references
+        """
+        filtered_references = []
+        for reference in references:
+            file_paths = [file[1] for file in filtered_references]
+            try:
+                index = file_paths.index(reference[1])
+                filtered_references[index][0].extend(reference[0])
+            except ValueError:
+                filtered_references.append(reference)
+
+        return filtered_references
+
+    @CommandBase.conform_command()
+    async def __call__(
+        self,
+        parameters: Dict[str, Any],
+        action_query: ActionQuery,
+        logger: logging.Logger,
+    ):
+        # All the references found will be stored in this variable
+        references_found: References = []
+
+        # Get all the references of the scene
+        scene_references = await Utils.wrapped_execute(action_query, hou.fileReferences)
+
+        # Loop over all the filtered references
+        for parameter, file_path in await self._filter_references(await scene_references, parameters["skip_conformed"]):
+            # Get the real path
+            expanded_path = await Utils.wrapped_execute(action_query, hou.expandString, file_path)
+            file_path = pathlib.Path(await expanded_path)
+
+            # Make sure the file path leads to a reachable file
+            skip = False
+            while not file_path.exists():
+                file_path, skip = await self._prompt_new_path(
+                    action_query, file_path, parameter
+                )
+                if skip or file_path is None:
+                    break
+            # The user can decide to skip the references that are not reachable
+            if skip or file_path is None:
+                logger.info("Skipping the reference at %s", parameter)
+                continue
+
+            # Special treatment for HDAs
+            hda_extensions = [".hda", ".hdanc", ".hdalc"]
+            if parameter is None and file_path.suffix in hda_extensions:
+                definitions = await Utils.wrapped_execute(
+                    action_query, hou.hda.definitionsInFile, file_path.as_posix()
+                )
+                references_found.append((await definitions, file_path))
+
+            # Look for file sequence
+            sequence = None
+            for file_sequence in fileseq.findSequencesOnDisk(str(file_path.parent)):
+                # Find the file sequence that correspond the to file we are looking for
+                sequence_list = [pathlib.Path(str(file)) for file in file_sequence]
+                if file_path in sequence_list and len(sequence_list) > 1:
+                    sequence = file_sequence
+                    file_path = sequence_list
+                    break
+
+            # Append to the references found
+            references_found.append(([parameter], file_path))
+            if sequence is None:
+                logger.info("Referenced file(s) %s found at %s", file_path, parameter)
+            else:
+                logger.info("Referenced file(s) %s found at %s", sequence, parameter)
+
+        # Remove all the duplicates
+        references_found = self._merge_duplicates(references_found)
+
+        # Display a message to the user to inform about all the references to conform
+        referenced_file_paths = [
+            fileseq.findSequencesInList(reference[1])
+            if isinstance(reference[1], list)
+            else [reference[1]]
+            for reference in references_found
+        ]
+        message = f"The scene\n{hou.hipFile.path()}\nis referencing non conformed file(s) :\n\n"
+        for file_path in referenced_file_paths:
+            message += f"- {' '.join([str(f) for f in file_path])}\n"
+
+        message += "\nThese files must be conformed and repathed first. Press continue to conform and repath them"
+        info_parameter = ParameterBuffer(
+            type=TextParameterMeta("info"),
+            name="info",
+            label="Info",
+            value=message,
+        )
+        # Send the message to the user
+        if referenced_file_paths:
+            await self.prompt_user(action_query, {"info": info_parameter})
+
+        return {
+            "attributes": [file[0] for file in references_found],
+            "file_paths": [file[1] for file in references_found],
+        }
